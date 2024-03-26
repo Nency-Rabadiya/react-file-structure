c3a108a84790426b8f0e265b433a1db0fded6072
Nency-Rabadiya <nency.rabadiya@exhibytesolution.com>
2024-03-26 18:25:58 +0530

    Form using ant design.

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
 import App from "./App.tsx";
 import reportWebVitals from "./reportWebVitals.ts";
 import "./index.css";
-import {store} from "./redux-toolkit-example/store/index.tsx"
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <Provider store = {store}>
+  <React.StrictMode>
     <App />
-  </Provider>
+  </React.StrictMode>
 );
 
 reportWebVitals();
