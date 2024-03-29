e2e14536a1ee873f44e3091d14f9ef6bf5d75945
Nency-Rabadiya <nency.rabadiya@exhibytesolution.com>
2024-03-29 18:32:42 +0530

    React test library and jest

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import TestExample from "./jest/index.tsx";
+import QueryBy from "./jest/QueryBy.tsx";
 
 const App: React.FC = () => (
   <div className="App">
     <TestExample />
+    <QueryBy name="xyz" surname="abcd" />
   </div>
 );
 
---
+++ src/jest/QueryWithinElement.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const QueryWithinElement: React.FC = () => (
+  <div>
+    Parent
+    <p>child1</p>
+    <p>child2</p>
+    <p>child3</p>
+  </div>
+);
+
+export default QueryWithinElement;
